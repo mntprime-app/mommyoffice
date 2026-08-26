2697ddd53402a86114fb963aba026650b22afefe
Amaraa <amaraa2434@gmail.com>
2026-08-26 18:44:33 +0800

    fix: setContent options for Tiptap v3 API

--- src/components/admin/RichTextEditor.tsx
+++ src/components/admin/RichTextEditor.tsx
@@ -44,7 +44,7 @@ export default function RichTextEditor({ value, onChange, placeholder = 'Ний�
   // Sync external value changes (e.g. edit page loading data)
   useEffect(() => {
     if (editor && value !== editor.getHTML()) {
-      editor.commands.setContent(value || '', false);
+      editor.commands.setContent(value || '', { emitUpdate: false });
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [value]);
